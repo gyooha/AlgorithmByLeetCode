d9fa32face63060e0af650fcb1e637db0080381f
dawn.kgh <dawn.kgh@kakaomobility.com>
2021-11-08 18:25:38 +0900

    solve no.23 problem

---
+++ src/main/kotlin/gyooha/MergeKList.kt
@@ -0,0 +1,34 @@
+package gyooha
+
+class MergeKList {
+    fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+        if (lists.isEmpty()) return null
+
+        val result = mutableListOf<Int>()
+        lists.forEach { list ->
+            var current: ListNode? = list
+
+            while (current != null) {
+                result.add(current.`val`)
+                current = current.next
+            }
+        }
+        if (result.isEmpty()) return null
+
+        result.sort()
+
+        val head = ListNode(0)
+        var dummy: ListNode? = head
+
+        result.forEach { value ->
+            dummy?.next = ListNode(value)
+            dummy = dummy?.next
+        }
+
+        return head.next
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+}
