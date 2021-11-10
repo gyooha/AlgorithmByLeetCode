04dbe29fc40bbfe7483094b1b8ddc47f6b7274ff
dawn.kgh <dawn.kgh@kakaomobility.com>
2021-11-10 11:14:21 +0900

    solve no.24 problem

---
+++ src/main/kotlin/gyooha/SwapNodesInPairs.kt
@@ -0,0 +1,70 @@
+package gyooha
+
+class SwapNodesInPairs {
+    fun swapPairs(head: ListNode?): ListNode? {
+        if (head == null) return null
+        if (head.next == null) return head
+
+        val first = mutableListOf<Int>()
+        val second = mutableListOf<Int>()
+
+        var current = head
+        var flag = false
+        while (current != null) {
+            when (flag) {
+                true -> second.add(current.`val`)
+                false -> first.add(current.`val`)
+            }
+            flag = !flag
+
+            current = current.next
+        }
+
+        val maxSize = if (second.size > first.size) second.size else first.size
+        val mergedList = mutableListOf<Int>()
+        for (i in 0..maxSize) {
+            if (second.getOrNull(i) != null) mergedList.add(second[i])
+            if (first.getOrNull(i) != null) mergedList.add(first[i])
+        }
+
+        val result = ListNode(0)
+        var dummy: ListNode? = result
+
+        mergedList.forEach {
+            dummy?.next = ListNode(it)
+            dummy = dummy?.next
+        }
+
+        return result.next
+    }
+
+    fun test(head: ListNode?): ListNode? {
+        if (head?.next == null) return head
+        val result = test(head.next?.next)
+        val newHead = head.next
+        newHead?.next = head
+        newHead?.next?.next = result
+        return newHead
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+}
+
+fun main() {
+    SwapNodesInPairs().apply {
+        test(
+            SwapNodesInPairs.ListNode(1).apply {
+                next = SwapNodesInPairs.ListNode(2).apply {
+                    next = SwapNodesInPairs.ListNode(3)
+                }
+            }
+        ).also {
+            println(it?.`val`)
+            println(it?.next?.`val`)
+            println(it?.next?.next?.`val`)
+            println(it?.next?.next?.next?.`val`)
+        }
+    }
+}
